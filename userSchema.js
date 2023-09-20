85b40f1598d4a62ee10aeb8dfb63934174407a9e
Raushan Kumar <122693547+aryanbaba4199@users.noreply.github.com>
2023-09-21 01:24:55 +0530

    Update userSchema.js

--- userSchema.js
+++ userSchema.js
@@ -6,9 +6,11 @@ const bookingSchema = new mongoose.Schema({
   address: String,
   mobile: String,
   selectedServices: [String],
+  functionType : String,
+  Detail : String,
 });
 
 // Create a model using the schema
 const Booking = mongoose.model('Booking', bookingSchema);
 
-module.exports = Booking; // Export the Booking model for use in other parts of your application
+module.exports = Booking;
